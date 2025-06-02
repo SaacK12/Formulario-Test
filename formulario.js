348a40632d3fb7a94082b438407406e565e0cf1b
SaacK <161529566+SaacK12@users.noreply.github.com>
2025-06-02 18:20:50 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -21,7 +21,7 @@ else{
     emailjs.send("service_eajnwps","template_bfsmb7m",usuario)
       alert("Enviado!")
   //APPScript test
-      fetch('AKfycbzjhL66ryiWxYHkb-i3kQYaIIDHHRg0kmoYuJTXI9lJRpPFWwHfG-sVHGKSajIpy65cjg', {
+      fetch('https://script.google.com/macros/s/AKfycbzjhL66ryiWxYHkb-i3kQYaIIDHHRg0kmoYuJTXI9lJRpPFWwHfG-sVHGKSajIpy65cjg/exec', {
   method: 'POST',
   body: JSON.stringify(usuario),
   headers: { 'Content-Type': 'application/json' }
